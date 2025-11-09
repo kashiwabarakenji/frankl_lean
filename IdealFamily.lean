02c38335c2ea0f94c8c80a83587932b7e39758bc
kashiwa <kashiwa@idea.c.u-tokyo.ac.jp>
2025-11-09 17:53:23 +0900

    importだけのファイルも

---
+++ IdealFamily.lean
@@ -0,0 +1,7 @@
+/- Umbrella module: users can `import IdealFamily` to get the main API. -/
+import IdealFamily.Core
+import IdealFamily.TraceGuarded
+import IdealFamily.Contraction
+import IdealFamily.NDSDiff
+import IdealFamily.Corollaries
+import IdealFamily.Proofs
